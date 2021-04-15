40b02a23ee26a3bb0cddeaf57e6159f9b56021b6
ccqstark <1367305698@qq.com>
2021-04-15 21:46:41 +0800

    sentinel nacos-config

---
+++ sentinel-sample/src/test/java/com/quanta/sentinelsample/SentinelSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanta.sentinelsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
