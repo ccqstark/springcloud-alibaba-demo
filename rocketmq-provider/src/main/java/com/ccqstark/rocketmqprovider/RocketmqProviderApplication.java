f05d2c971e1eab58db9d77e283a5ed1ee7efe95c
ccqstark <1367305698@qq.com>
2021-04-19 23:10:42 +0800

    🚀RocketMQ

---
+++ rocketmq-provider/src/main/java/com/ccqstark/rocketmqprovider/RocketmqProviderApplication.java
@@ -0,0 +1,13 @@
+package com.ccqstark.rocketmqprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqProviderApplication.class, args);
+    }
+
+}
